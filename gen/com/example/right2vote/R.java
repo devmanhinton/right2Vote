2dbc83f0fd5f4a78ef5987b76d9b105c6ad21495
Devon Hinton <dhinton@stanford.edu>
2014-11-30 23:05:21 -0800

    new home screen, boom

--- gen/com/example/right2vote/R.java
+++ gen/com/example/right2vote/R.java
@@ -10,6 +10,9 @@ package com.example.right2vote;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int red=0x7f060000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -22,47 +25,48 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int border=0x7f020000;
-        public static final int hiliary_clinton=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int issues_red=0x7f020003;
-        public static final int logistics_red=0x7f020004;
-        public static final int logo_white=0x7f020005;
-        public static final int my_ballot_red=0x7f020006;
-        public static final int my_voting_location=0x7f020007;
-        public static final int poster=0x7f020008;
-        public static final int share_red=0x7f020009;
-        public static final int ted_cruz=0x7f02000a;
-        public static final int voter=0x7f02000b;
+        public static final int background_image=0x7f020000;
+        public static final int border=0x7f020001;
+        public static final int button_1=0x7f020002;
+        public static final int button_2=0x7f020003;
+        public static final int button_3=0x7f020004;
+        public static final int button_4=0x7f020005;
+        public static final int hiliary_clinton=0x7f020006;
+        public static final int ic_launcher=0x7f020007;
+        public static final int issues_red=0x7f020008;
+        public static final int logistics_red=0x7f020009;
+        public static final int logo_white=0x7f02000a;
+        public static final int my_ballot_red=0x7f02000b;
+        public static final int my_voting_location=0x7f02000c;
+        public static final int poster=0x7f02000d;
+        public static final int share_red=0x7f02000e;
+        public static final int ted_cruz=0x7f02000f;
+        public static final int voter=0x7f020010;
     }
     public static final class id {
-        public static final int action_settings=0x7f08001a;
-        public static final int agree=0x7f080009;
-        public static final int cruzBox=0x7f080011;
-        public static final int cruzImage=0x7f080012;
-        public static final int cruzText=0x7f080015;
-        public static final int disagree=0x7f08000a;
-        public static final int finished=0x7f08000e;
-        public static final int hilaryBox=0x7f08000f;
-        public static final int hilaryText=0x7f080014;
-        public static final int hiliaryImage=0x7f080010;
-        public static final int homeButton=0x7f08000b;
-        public static final int image1=0x7f080001;
-        public static final int image2=0x7f080003;
-        public static final int image3=0x7f080005;
-        public static final int image4=0x7f080007;
-        public static final int issuesButton=0x7f08000d;
-        public static final int logisticsDate=0x7f080017;
-        public static final int logisticsDateLabel=0x7f080016;
-        public static final int logisticsMap=0x7f080019;
-        public static final int logisticsMapLabel=0x7f080018;
-        public static final int notFinished=0x7f08000c;
-        public static final int policyStatement=0x7f080008;
-        public static final int textIssues=0x7f080000;
-        public static final int textLogistics=0x7f080004;
-        public static final int textMyBallot=0x7f080002;
-        public static final int textShare=0x7f080006;
-        public static final int winner=0x7f080013;
+        public static final int action_settings=0x7f090016;
+        public static final int agree=0x7f090005;
+        public static final int button1=0x7f090000;
+        public static final int button2=0x7f090001;
+        public static final int button3=0x7f090002;
+        public static final int button4=0x7f090003;
+        public static final int cruzBox=0x7f09000d;
+        public static final int cruzImage=0x7f09000e;
+        public static final int cruzText=0x7f090011;
+        public static final int disagree=0x7f090006;
+        public static final int finished=0x7f09000a;
+        public static final int hilaryBox=0x7f09000b;
+        public static final int hilaryText=0x7f090010;
+        public static final int hiliaryImage=0x7f09000c;
+        public static final int homeButton=0x7f090007;
+        public static final int issuesButton=0x7f090009;
+        public static final int logisticsDate=0x7f090013;
+        public static final int logisticsDateLabel=0x7f090012;
+        public static final int logisticsMap=0x7f090015;
+        public static final int logisticsMapLabel=0x7f090014;
+        public static final int notFinished=0x7f090008;
+        public static final int policyStatement=0x7f090004;
+        public static final int winner=0x7f09000f;
     }
     public static final class layout {
         public static final int activity_go_home=0x7f030000;
@@ -73,43 +77,43 @@ public final class R {
         public static final int activity_voting_logistics=0x7f030005;
     }
     public static final class menu {
-        public static final int ballot=0x7f070000;
-        public static final int go_home=0x7f070001;
-        public static final int main=0x7f070002;
-        public static final int policy_statement=0x7f070003;
-        public static final int show_ballot=0x7f070004;
-        public static final int show_candidate=0x7f070005;
-        public static final int show_winner=0x7f070006;
-        public static final int voting_logistics=0x7f070007;
+        public static final int ballot=0x7f080000;
+        public static final int go_home=0x7f080001;
+        public static final int main=0x7f080002;
+        public static final int policy_statement=0x7f080003;
+        public static final int show_ballot=0x7f080004;
+        public static final int show_candidate=0x7f080005;
+        public static final int show_winner=0x7f080006;
+        public static final int voting_logistics=0x7f080007;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
-        public static final int agree=0x7f050010;
+        public static final int agree=0x7f05000d;
         public static final int app_name=0x7f050000;
         public static final int button_send=0x7f050003;
-        public static final int cruzChosenText=0x7f05001a;
-        public static final int disagree=0x7f050011;
+        public static final int cruzChosenText=0x7f050013;
+        public static final int disagree=0x7f05000e;
         public static final int hello_world=0x7f050001;
-        public static final int hilaryChosenText=0x7f050019;
-        public static final int home_button=0x7f05000d;
+        public static final int hilaryChosenText=0x7f050012;
+        public static final int home_button=0x7f05000c;
         public static final int image_desc=0x7f050004;
         public static final int issues=0x7f050005;
-        public static final int issues_button=0x7f050017;
+        public static final int issues_button=0x7f050010;
         public static final int logistics=0x7f050007;
-        public static final int logistics_date=0x7f05000c;
-        public static final int logistics_date_label=0x7f05000b;
-        public static final int logistics_map_label=0x7f05000a;
+        public static final int logistics_date=0x7f05000b;
+        public static final int logistics_date_label=0x7f05000a;
+        public static final int logistics_map_label=0x7f050009;
         public static final int myBallot=0x7f050006;
         public static final int share=0x7f050008;
-        public static final int still_statements=0x7f050016;
-        public static final int title_activity_ballot=0x7f050015;
-        public static final int title_activity_go_home=0x7f05000f;
-        public static final int title_activity_policy_statement=0x7f05000e;
-        public static final int title_activity_show_ballot=0x7f050014;
-        public static final int title_activity_show_candidate=0x7f050012;
-        public static final int title_activity_show_winner=0x7f050013;
-        public static final int title_activity_voting_logistics=0x7f050009;
-        public static final int winningCandidateTitle=0x7f050018;
+        public static final int still_statements=0x7f05000f;
+        public static final int title_activity_ballot=0x7f050019;
+        public static final int title_activity_go_home=0x7f050015;
+        public static final int title_activity_policy_statement=0x7f050014;
+        public static final int title_activity_show_ballot=0x7f050018;
+        public static final int title_activity_show_candidate=0x7f050016;
+        public static final int title_activity_show_winner=0x7f050017;
+        public static final int title_activity_voting_logistics=0x7f05001a;
+        public static final int winningCandidateTitle=0x7f050011;
     }
     public static final class style {
         /** 
@@ -133,10 +137,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
