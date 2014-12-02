4c8d7f44b9ba62feb1c1bb2bbb3c8340b1d54e57
Devon Hinton <dhinton@stanford.edu>
2014-12-02 09:31:41 -0800

    remove unused photos

--- gen/com/example/right2vote/R.java
+++ gen/com/example/right2vote/R.java
@@ -33,15 +33,11 @@ public final class R {
         public static final int button_4=0x7f020005;
         public static final int hiliary_clinton=0x7f020006;
         public static final int ic_launcher=0x7f020007;
-        public static final int issues_red=0x7f020008;
-        public static final int logistics_red=0x7f020009;
-        public static final int logo=0x7f02000a;
-        public static final int logo_white=0x7f02000b;
-        public static final int my_ballot_red=0x7f02000c;
-        public static final int my_voting_location=0x7f02000d;
-        public static final int poster=0x7f02000e;
-        public static final int share_red=0x7f02000f;
-        public static final int ted_cruz=0x7f020010;
+        public static final int logo=0x7f020008;
+        public static final int logo_white=0x7f020009;
+        public static final int my_voting_location=0x7f02000a;
+        public static final int poster=0x7f02000b;
+        public static final int ted_cruz=0x7f02000c;
     }
     public static final class id {
         public static final int action_settings=0x7f090017;
